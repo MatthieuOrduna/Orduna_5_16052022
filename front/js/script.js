74f32eb48b2f9e8bff4df9ed2e2ce3c21e7e4982
Matthieu ORDUNA <orduna.developpeurweb@gmail.com>
2022-05-20 13:17:12 +0100

    Index

--- front/js/script.js
+++ front/js/script.js
@@ -1,26 +1,28 @@
-fetch ("http://localhost:3000/api/products")
-.then((res)=> {
-        console.log("raw data",res);
-        return res.json();
+fetch ("http://localhost:3000/api/products") 
+//Fetch est une méthode du langage JS dont on se sert pour appeler (user) l'API du projet via une requête HTTP.
+.then((res)=> { //res = response (c'est une promesse de réponse).
+        console.log("raw data",res); //console.log nous permet de voir "res".
+        return res.json(); //La réponse attendue sera alors transformé en .json en JavaScript.
 })
 .then((products)=> {
     let items = document.getElementById('items');
     console.log("items",items);
+    //On retrouve l'élément "items".
     console.log("products",products);
-    products.forEach (product =>{
-        a = document.createElement("a");
+    products.forEach (product =>{ //On boucle pour obtenir tous les produits en une fois.
+        a = document.createElement("a");//On créer de nouveau élément.
         article = document.createElement("article");
         img = document.createElement("img");
         h3 = document.createElement("h3");
-        h3.classList.add("productName");
+        h3.classList.add("productName");//On ajoute des classes.
         p = document.createElement("p");
         p.classList.add("productDescription");
 
-        items.appendChild(a);
+        items.appendChild(a); //On ajoute des enfants à "items".
         a.appendChild(article);
         [img,h3,p].forEach(element=>article.appendChild(element));
 
-
+        a.setAttribute("href","product.html")
 
         //<a href="./product.html?id=42">
             //<article>
